387922953fa14dc7020c0855f38c107d46d220e6
Mustafa Ryad <mustafariad03@gmail.com>
2026-09-05 07:07:40 +0300

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.js
@@ -0,0 +1,20 @@
+class NumArray {
+    /**
+     * @param {number[]}
+     */
+    constructor(nums) {
+        this.prefix = new Array(nums.length + 1).fill(0);
+        for (let i = 0; i < nums.length; i++) {
+            this.prefix[i + 1] = nums[i] + this.prefix[i];
+        }
+    }
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        return this.prefix[right + 1] - this.prefix[left]
+    }
+}
